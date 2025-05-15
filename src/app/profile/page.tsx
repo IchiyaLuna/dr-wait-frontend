ca232a1e0decbfe458549e30aa182327d133421c
Ichiya Luna <ichiyaluna@yonsei.ac.kr>
2025-05-15 17:40:58 +0900

    ✨ Example reservation and profile page updated
    + Example profile page.tsx
    + Example profile page style module
    + Example reservation page.tsx
    + Example reservation page style module

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,33 @@
+import TabBar from '@/components/TabBar';
+// Styles
+import styles from './page.module.scss';
+// Page
+export default function ProfilePage() {
+  return (
+    <>
+      <main className={styles.main}>
+        <div className={styles.component}>MyPage</div>
+        <div className={styles.component}>MyPage</div>
+        <div className={styles.component}>MyPage</div>
+        <div className={styles.component}>MyPage</div>
+        <div className={styles.component}>MyPage</div>
+        <div className={styles.component}>MyPage</div>
+        <div className={styles.component}>MyPage</div>
+        <div className={styles.component}>MyPage</div>
+        <div className={styles.component}>MyPage</div>
+        <div className={styles.component}>MyPage</div>
+        <div className={styles.component}>MyPage</div>
+        <div className={styles.component}>MyPage</div>
+        <div className={styles.component}>MyPage</div>
+        <div className={styles.component}>MyPage</div>
+        <div className={styles.component}>MyPage</div>
+        <div className={styles.component}>MyPage</div>
+        <div className={styles.component}>MyPage</div>
+        <div className={styles.component}>MyPage</div>
+        <div className={styles.component}>MyPage</div>
+        <div className={styles.component}>MyPage</div>
+      </main>
+      <TabBar />
+    </>
+  );
+}
---
+++ src/app/reservation/page.tsx
@@ -0,0 +1,33 @@
+import TabBar from '@/components/TabBar';
+// Styles
+import styles from './page.module.scss';
+// Page
+export default function ReservationPage() {
+  return (
+    <>
+      <main className={styles.main}>
+        <div className={styles.component}>Reservation</div>
+        <div className={styles.component}>Reservation</div>
+        <div className={styles.component}>Reservation</div>
+        <div className={styles.component}>Reservation</div>
+        <div className={styles.component}>Reservation</div>
+        <div className={styles.component}>Reservation</div>
+        <div className={styles.component}>Reservation</div>
+        <div className={styles.component}>Reservation</div>
+        <div className={styles.component}>Reservation</div>
+        <div className={styles.component}>Reservation</div>
+        <div className={styles.component}>Reservation</div>
+        <div className={styles.component}>Reservation</div>
+        <div className={styles.component}>Reservation</div>
+        <div className={styles.component}>Reservation</div>
+        <div className={styles.component}>Reservation</div>
+        <div className={styles.component}>Reservation</div>
+        <div className={styles.component}>Reservation</div>
+        <div className={styles.component}>Reservation</div>
+        <div className={styles.component}>Reservation</div>
+        <div className={styles.component}>Reservation</div>
+      </main>
+      <TabBar />
+    </>
+  );
+}
