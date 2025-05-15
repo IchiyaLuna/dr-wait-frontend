88b495aecebd07fff6282e94cb9bd0b3fc113e85
Ichiya Luna <ichiyaluna@yonsei.ac.kr>
2025-05-15 16:06:03 +0900

    🎨 PretendardJP settings moved to layout.tsx
    - settings in globals.scss
    + settings in layout.tsx
    + style module of layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,31 @@
-import type { Metadata } from "next";
-import "./globals.scss";
-
+// React
+import React from 'react';
+// Metadata
+import type { Metadata } from 'next';
+// Font
+import localFont from 'next/font/local';
+// Styles
+import './globals.scss';
+import styles from './layout.module.scss';
+// Metadata
 export const metadata: Metadata = {
-  title: "Dr.Wait",
-  description: "-",
+  title: 'Dr.Wait',
+  description: '-',
 };
-
+// Pretendard font
+const pretendard = localFont({
+  src: '../assets/fonts/PretendardJPVariable.woff2',
+  display: 'swap',
+  weight: '45 920',
+});
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ko">
-      <body
-        className={''}
-      >
-        {children}
-      </body>
+    <html lang="ko" className={pretendard.className}>
+      <body className={styles.body}>{children}</body>
     </html>
   );
 }
