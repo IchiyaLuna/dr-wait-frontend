57c1eef5ab58e06d836d8f1e24dc0f65ec45893f
Ichiya Luna <ichiyaluna@yonsei.ac.kr>
2025-05-17 14:27:02 +0900

    🔧 Tailwind config updated
    - no more color vars adder
    + just use var(color var name)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 const config = {
-    content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
-    theme: {
-        extend: {},
-    },
-    plugins: [
-        function ({ addBase, theme }) {
-            function extractColorVars(colorObj, colorGroup = '') {
-                return Object.keys(colorObj).reduce((vars, colorKey) => {
-                    const value = colorObj[colorKey];
-                    const cssVariable =
-                        colorKey === 'DEFAULT'
-                            ? `--color${colorGroup}`
-                            : `--color${colorGroup}-${colorKey}`;
-
-                    const newVars =
-                        typeof value === 'string'
-                            ? { [cssVariable]: value }
-                            : extractColorVars(value, `-${colorKey}`);
-
-                    return { ...vars, ...newVars };
-                }, {});
-            }
-
-            addBase({
-                ':root': extractColorVars(theme('colors')),
-            });
-        },
-    ],
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    // function ({ addBase, theme }) {
+    //     function extractColorVars(colorObj, colorGroup = '') {
+    //         return Object.keys(colorObj).reduce((vars, colorKey) => {
+    //             const value = colorObj[colorKey];
+    //             const cssVariable =
+    //                 colorKey === 'DEFAULT'
+    //                     ? `--color${colorGroup}`
+    //                     : `--color${colorGroup}-${colorKey}`;
+    //
+    //             const newVars =
+    //                 typeof value === 'string'
+    //                     ? { [cssVariable]: value }
+    //                     : extractColorVars(value, `-${colorKey}`);
+    //
+    //             return { ...vars, ...newVars };
+    //         }, {});
+    //     }
+    //
+    //     addBase({
+    //         ':root': extractColorVars(theme('colors')),
+    //     });
+    // },
+  ],
 };
 export default config;
