fdd482be359e85bf317069b88f95bbe205427b76
Ichiya Luna <ichiyaluna@yonsei.ac.kr>
2025-05-21 18:24:49 +0900

    🏷️ kakaomaps search.ts type added
    + searchResult interface added

---
+++ src/types/kakao/maps/search.ts
@@ -0,0 +1,14 @@
+export interface SearchResult {
+  id: string;
+  place_name: string;
+  category_name: string;
+  category_group_code: string;
+  category_group_name: string;
+  phone: string;
+  address_name: string;
+  road_address_name: string;
+  x: number;
+  y: number;
+  place_url: string;
+  distance?: string;
+}
