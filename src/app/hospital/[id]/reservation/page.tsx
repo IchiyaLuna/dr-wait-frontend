3d76b3cef74c2f8c2830d976171a42afcf96d080
Ichiya Luna <ichiyaluna@yonsei.ac.kr>
2025-05-25 22:13:36 +0900

    ✨ (temp) HospitalReservation page added
    + (temp) HospitalReservation page.tsx added
    + (temp) HospitalReservation style module added

---
+++ src/app/hospital/[id]/reservation/page.tsx
@@ -0,0 +1,35 @@
+// React
+import React from 'react';
+// Components
+import TopBar from '@/components/TopBar';
+import ProfileCard from '@/components/ProfileCard';
+import Row from '@/components/Row';
+import Card from '@/components/Card';
+import SectionTitle from '@/components/SectionTitle';
+import RowScroll from '@/components/RowScroll';
+import DepartmentCard from '@/components/DepartmentCard';
+import TabBar from '@/components/TabBar';
+// Styles
+import styles from './page.module.scss';
+// Page
+export default function HospitalReservationPage() {
+  return (
+    <>
+      <TopBar type={'BACK'} />
+      <main className={styles.main}>
+        <Card>
+          <h2>진료대상 선택</h2>
+          <div>
+            <Card>유저1</Card>
+            <Card>유저2</Card>
+          </div>
+        </Card>
+        <Card>진료항목 선택</Card>
+        <Card>증상 설명</Card>
+      </main>
+      <div className={styles.footer}>
+        <button className={styles.checkIn}>진료 접수하기</button>
+      </div>
+    </>
+  );
+}
