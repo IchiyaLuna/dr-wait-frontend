4d41f82fa918ea10ac5fe15ac53c5b0b13bdeaf4
Ichiya Luna <ichiyaluna@yonsei.ac.kr>
2025-05-25 22:12:41 +0900

    ✨ TopBar component updated
    + 'LOGO' and 'BACK varient added

--- src/components/TopBar/index.tsx
+++ src/components/TopBar/index.tsx
@@ -3,23 +3,35 @@ import styles from './index.module.scss';
 // Props
 type Props = {
   title?: string;
+  type: 'LOGO' | 'BACK';
 };
 // Component
-export default function TopBar({ title }: Props) {
+export default function TopBar({ title, type = 'LOGO' }: Props) {
   return (
     <div className={styles.wrapper}>
-      <div>
-        {title ? (
-          title
-        ) : (
-          <span className={styles.logo}>
-            Dr.Wait<sup>+</sup>
-          </span>
-        )}
-      </div>
-      <div>
-        <i className="fa-solid fa-bars"></i>
-      </div>
+      {type === 'LOGO' ? (
+        <div>
+          {title ? (
+            title
+          ) : (
+            <span className={styles.logo}>
+              Dr.Wait<sup>+</sup>
+            </span>
+          )}
+        </div>
+      ) : null}
+      {type === 'BACK' ? (
+        <div>
+          <button className={styles.back}>
+            <i className={`fa-fw fa-regular fa-chevron-left`} />
+          </button>
+        </div>
+      ) : null}
+      {type === 'LOGO' ? (
+        <div>
+          <i className="fa-solid fa-bars"></i>
+        </div>
+      ) : null}
     </div>
   );
 }
