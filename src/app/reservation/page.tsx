5411bd5231d1355f7c5ad192a1a46c12c2426a74
Ichiya Luna <ichiyaluna@yonsei.ac.kr>
2025-06-01 16:02:22 +0900

    ✨ Reservation Page updated

--- src/app/reservation/page.tsx
+++ src/app/reservation/page.tsx
@@ -1,3 +1,4 @@
+import TopBar from '@/components/TopBar';
 import TabBar from '@/components/TabBar';
 // Styles
 import styles from './page.module.scss';
@@ -5,27 +6,26 @@ import styles from './page.module.scss';
 export default function ReservationPage() {
   return (
     <>
+      <TopBar type={'LOGO'} />
       <main className={styles.main}>
-        <div className={styles.component}>Reservation</div>
-        <div className={styles.component}>Reservation</div>
-        <div className={styles.component}>Reservation</div>
-        <div className={styles.component}>Reservation</div>
-        <div className={styles.component}>Reservation</div>
-        <div className={styles.component}>Reservation</div>
-        <div className={styles.component}>Reservation</div>
-        <div className={styles.component}>Reservation</div>
-        <div className={styles.component}>Reservation</div>
-        <div className={styles.component}>Reservation</div>
-        <div className={styles.component}>Reservation</div>
-        <div className={styles.component}>Reservation</div>
-        <div className={styles.component}>Reservation</div>
-        <div className={styles.component}>Reservation</div>
-        <div className={styles.component}>Reservation</div>
-        <div className={styles.component}>Reservation</div>
-        <div className={styles.component}>Reservation</div>
-        <div className={styles.component}>Reservation</div>
-        <div className={styles.component}>Reservation</div>
-        <div className={styles.component}>Reservation</div>
+        <div>
+          <div>진행중</div>
+          <div>과거 진료내역</div>
+        </div>
+        <div>
+          <div className={styles.reservationCard}>
+            <h3>병원 이름</h3>
+            <p>진료과목</p>
+            <p>접수 시간</p>
+            <p>진료 시간</p>
+          </div>
+          <div className={styles.reservationCard}>
+            <h3>병원 이름</h3>
+            <p>진료과목</p>
+            <p>접수 시간</p>
+            <p>진료 시간</p>
+          </div>
+        </div>
       </main>
       <TabBar />
     </>
