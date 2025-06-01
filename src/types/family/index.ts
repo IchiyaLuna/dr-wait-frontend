82d8be9775bff48cadfcc36170a079bc0fd55468
Ichiya Luna <ichiyaluna@yonsei.ac.kr>
2025-06-01 16:00:27 +0900

    🔖 Types updated

---
+++ src/types/family/index.ts
@@ -0,0 +1,8 @@
+export interface FamilyMember {
+  familyGroupId: number;
+  userId: string;
+  fullname: string;
+  role: string;
+  phoneNumber: string;
+  isConfirmed: boolean;
+}
---
+++ src/types/hospital/index.ts
@@ -0,0 +1,8 @@
+export interface Hospital {
+  id: string;
+  name: string;
+  address: string;
+  telephone: string;
+  websiteUrl: string;
+  department: string;
+}
