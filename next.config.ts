93cf4c80f6d449f6d6389d1beba021dc486b2d50
Ichiya Luna <ichiyaluna@yonsei.ac.kr>
2025-06-01 16:04:20 +0900

    🔧 Next.js config updated

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   devIndicators: false,
-  reactStrictMode: false,
 };
 
 export default nextConfig;
